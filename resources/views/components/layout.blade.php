e57d150780cf132d21c7e242a68b58b5c8a5d171
Jeremy Charbonnel <jeremy.charbonn3l@gmail.com>
2024-07-23 14:45:24 +0200

    Added build, assets bundling with vite

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,7 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel example - {{ $heading }}</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+    @vite(['resources/js/app.js'])
+
+    {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
 </head>
 
 <body class="h-full">
--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -8,7 +8,7 @@
             <a href="/jobs/{{ $job->id }}" class="block px-4 py-6 border border-gray-200 rounded-md">
                 <div class="font-bold text-blue-400">{{ $job->employer->name }}</div>
                 <div>
-                    <strong>{{ $job->title }}</strong>: {{ $job->salary }} per year.
+                    <strong class="text-laracasts">{{ $job->title }}</strong>: {{ $job->salary }} per year.
                 </div>
             </a>
         @endforeach
