d4c803a0442db793c8ffd1d642f7881458b164de
Jeremy Charbonnel <jeremy.charbonn3l@gmail.com>
2024-07-23 14:33:25 +0200

    Added queue and job

---
+++ app/Jobs/TranslateJob.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Job;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class TranslateJob implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(public Job $jobListing)
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        // logger('Hello fron TranslateJob');
+
+        logger('Translating ' . $this->jobListing->title . ' to Spanish ...');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,21 @@
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
-use App\Mail\JobPosted;
-use Illuminate\Support\Facades\Mail;
+use App\Jobs\TranslateJob;
+use App\Models\Job;
 use Illuminate\Support\Facades\Route;
+
+Route::get('test', function() {
+    // dispatch(function() {
+    //     logger('hello from the queue!');
+    // })->delay(5);
+
+    $job = Job::first();
+
+    TranslateJob::dispatch($job);
+
+    return 'Done';
+});
 
 Route::get('/', function () {
     return view('home', [
